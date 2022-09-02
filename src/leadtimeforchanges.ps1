661b0eb2212e8a8a8ff282d0f8e0df5382817fb4
Sam Smith <samsmithnz@github.com>
2022-09-02 01:16:36 +0000

    Updated codespaces container to have default query

--- src/leadtimeforchanges.ps1
+++ src/leadtimeforchanges.ps1
@@ -1,1 +1,21 @@
+Param(
+    [string] $ownerRepo,
+    [string] $workflows,
+    [string] $branch,
+    [Int32] $numberOfDays
+)
+
+#==========================================
+#Input processing
+$ownerRepoArray = $ownerRepo -split '/'
+$owner = $ownerRepoArray[0]
+$repo = $ownerRepoArray[1]
+Write-Output "Owner/Repo: $owner/$repo"
+$workflowsArray = $workflows -split ','
+Write-Output "Workflows: $($workflowsArray[0])"
+Write-Output "Branch: $branch"
+$numberOfDays = $numberOfDays        
+Write-Output "Number of days: $numberOfDays"
+
+
 Write-Output "hello world"
