0980be9759488c310e9ad0e8735f8157730e6060
Sam Smith <samsmithnz@github.com>
2022-09-02 20:07:17 +0000

    Added commit scanning

--- src/leadtimeforchanges.ps1
+++ src/leadtimeforchanges.ps1
@@ -5,6 +5,8 @@ Param(
     [Int32] $numberOfDays
 )
 
+CLS
+
 #==========================================
 #Input processing
 $ownerRepoArray = $ownerRepo -split '/'
@@ -16,6 +18,31 @@ Write-Output "Workflows: $($workflowsArray[0])"
 Write-Output "Branch: $branch"
 $numberOfDays = $numberOfDays        
 Write-Output "Number of days: $numberOfDays"
+
+
+#Get the pull requests
+
+#https://developer.GitHub.com/v3/pulls/#list-pull-requests
+#GET /repos/:owner/:repo/pulls
+$uri = "https://api.github.com/repos/$owner/$repo/pulls?state=all&head=$branch&per_page=100";
+Write-Output $uri
+$prsResponse = Invoke-RestMethod -Uri $uri -ContentType application/json -Method Get -ErrorAction Stop
+
+Foreach ($pr in $prsResponse){
+            $url2 = "https://api.github.com/repos/$owner/$repo/pulls/$($pr.number)/commits?per_page=100";
+            Write-Output $url2
+            $prCommitsresponse = Invoke-RestMethod -Uri $url2 -ContentType application/json -Method Get -ErrorAction Stop
+            $prCommitsresponse.Length
+            # if (string.IsNullOrEmpty(response) == false)
+            # {
+            #     dynamic buildListObject = JsonConvert.DeserializeObject(response);
+            #     list = buildListObject;
+            # }
+}
+
+#$prsResponse   
+
+
 
 
 Write-Output "hello world"
