ed874910cc0b9b8d05e342b93c68c8e7ef500f6d
Sam Smith <samsmithnz@github.com>
2022-09-03 13:40:29 +0000

    Added PR + Commit basic time span measurement

--- src/leadtimeforchanges.ps1
+++ src/leadtimeforchanges.ps1
@@ -20,10 +20,8 @@ $numberOfDays = $numberOfDays
 Write-Output "Number of days: $numberOfDays"
 
 
-#Get the pull requests
-
+#Get pull requests from the repo 
 #https://developer.GitHub.com/v3/pulls/#list-pull-requests
-#GET /repos/:owner/:repo/pulls
 $uri = "https://api.github.com/repos/$owner/$repo/pulls?state=all&head=$branch&per_page=100";
 Write-Output $uri
 $prsResponse = Invoke-RestMethod -Uri $uri -ContentType application/json -Method Get -ErrorAction Stop
@@ -32,17 +30,15 @@ Foreach ($pr in $prsResponse){
             $url2 = "https://api.github.com/repos/$owner/$repo/pulls/$($pr.number)/commits?per_page=100";
             Write-Output $url2
             $prCommitsresponse = Invoke-RestMethod -Uri $url2 -ContentType application/json -Method Get -ErrorAction Stop
-            $prCommitsresponse.Length
-            # if (string.IsNullOrEmpty(response) == false)
-            # {
-            #     dynamic buildListObject = JsonConvert.DeserializeObject(response);
-            #     list = buildListObject;
-            # }
+            if ($prCommitsresponse.Length -ge 1)
+            {
+                $startDate = $prCommitsresponse[0].committer.date
+            }
+            if ($pr.Status -eq "closed" -and $pr.merged_at -ne $null)
+            {
+                $prTimeDuration = New-TimeSpan –Start $startDate –End $pr.merged_at
+            }
+            break
 }
 
-#$prsResponse   
-
-
-
-
-Write-Output "hello world"
+Write-Output "PR time duration $prTimeDuration"
